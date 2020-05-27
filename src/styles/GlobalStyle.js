195e48175c4ea1e86746604be3171c1767bbbc86
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-05-27 02:02:01 +0200

    StyledComponents GlobalStyle.js & theme.js

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 400;
+        src: url('../fonts/open-sans-v13-latin-regular.eot'); /* IE9 Compat Modes */
+        src: local('Open Sans'), local('OpenSans'),
+            url('../fonts/open-sans-v13-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+            url('../fonts/open-sans-v13-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+            url('../fonts/open-sans-v13-latin-regular.woff') format('woff'), /* Modern Browsers */
+            url('../fonts/open-sans-v13-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+            url('../fonts/open-sans-v13-latin-regular.svg#OpenSans') format('svg'); /* Legacy iOS */
+    }
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        outline: 0;
+    }
+
+    html,
+    body {
+        height: 100vh;
+        font-family: "Open Sans";
+        text-rendering: optimizelegibility;
+        -webkit-font-smoothing: antialiased;
+    }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+const AccentCyan = '#32e7b6';
+const AccentPink = '#ff40a7';
+
+const theme = {
+  colors: {
+    cyan: AccentCyan,
+    pink: AccentPink,
+    white: '#FFFFFF',
+    whiteCloud: '#F6f6f6',
+    black: 'black',
+  },
+
+  transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
+};
+
+export default theme;
