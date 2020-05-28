a6bd20592b915f8728f4e6427920d877404a1249
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-05-28 12:36:39 +0200

    Carousel Bug Fix | Thumbnail Width Calc

--- src/components/carouselCards.js
+++ src/components/carouselCards.js
@@ -1,14 +1,28 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
-const StyledLink = styled.a`
-  height: 400px;
-`;
-
 const CarouselCards = (props) => {
   const { items } = props;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  });
+
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
+  };
+
+  const calcThumbWidth = () => {
+    return (
+      -0.00008765044479330194 * width * width +
+      0.24421768707482994 * width -
+      30.531299058084773
+    );
+  };
+
   let images = items.filter(function (item) {
     return item.type === "image";
   });
@@ -18,14 +32,14 @@ const CarouselCards = (props) => {
       showThumbs={true}
       swipeable={true}
       showStatus={false}
-      infiniteLoop={true}
       emulateTouch={true}
+      thumbWidth={calcThumbWidth()}
     >
       {images.map((item, index) => {
         return (
-          <StyledLink href={item.link}>
+          <a key={index} href={item.link}>
             <img src={item.src} alt="Butopea" />
-          </StyledLink>
+          </a>
         );
       })}
     </Carousel>
