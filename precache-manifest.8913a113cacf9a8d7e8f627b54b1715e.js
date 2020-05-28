f15996bfe862aba6e2fcc1566a15e8a0c22c2fe9
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-05-28 02:04:16 +0200

    Updates

---
+++ precache-manifest.8913a113cacf9a8d7e8f627b54b1715e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce928aedddef83213b748fd6a117907",
+    "url": "/ButopeaTasks/index.html"
+  },
+  {
+    "revision": "bd0a15bc8efaa60c17b4",
+    "url": "/ButopeaTasks/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "bd0a15bc8efaa60c17b4",
+    "url": "/ButopeaTasks/static/js/2.7040962b.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/ButopeaTasks/static/js/2.7040962b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc73ccc257918c53b95",
+    "url": "/ButopeaTasks/static/js/main.bbe102d0.chunk.js"
+  },
+  {
+    "revision": "7802a2eaea8e4efacffd",
+    "url": "/ButopeaTasks/static/js/runtime-main.07417c7e.js"
+  }
+]);
